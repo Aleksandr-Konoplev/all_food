ecbda8e6fdabad13e44b32a791d8d425f5c29701
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-13 14:42:44 +1000

    fix admin.py in table_reservation

--- table_reservation/admin.py
+++ table_reservation/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from table_reservation.models import Reservation
+from table_reservation.models import Reservation, Table
 
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     list_display = ('id', 'client', 'date_visit', 'time_visit')
     list_filter = ('table', 'time_visit', 'date_visit')
-    search_fields = ('client',)
+    search_fields = ('client__email',)
+    ordering = ('date_visit', 'time_visit',)
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('id', 'num_table', 'num_of_seats')
+    list_filter = ('num_of_seats',)
