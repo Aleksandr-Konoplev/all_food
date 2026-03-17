8af72158b97a987e5f368426487774614f983801
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-18 02:58:20 +1000

    add widjet flatpickr in tamplate

--- table_reservation/forms.py
+++ table_reservation/forms.py
@@ -1,35 +1,45 @@
 from django import forms
 
-from table_reservation.models import Reservation
+from table_reservation.models import Reservation, Table
 
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ('start_at', 'end_at', 'table', 'deposit')
-        widgets = {
-            'start_at': forms.DateTimeInput(attrs={'class': 'form-control', 'step': 900}, format="%Y-%m-%dT%H:%M",),
-            'end_at': forms.DateTimeInput(attrs={'class': 'form-control', 'step': 900}, format="%Y-%m-%dT%H:%M",),
-            'table': forms.Select(attrs={'class': 'form-select'}),
-            'deposit': forms.NumberInput(attrs={'class': 'form-control', 'min': 0, 'step': 500}),
-        }
+        fields = ("start_at", "end_at", "table", "deposit")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["start_at"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Выберите дату и время начала брони", "step": 900}
+        )
+        self.fields["end_at"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Выберите дату и время окончания брони", "step": 900}
+        )
+        self.fields["table"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Выберите стол"}
+        )
+        self.fields["deposit"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Укажите депозит"}
+        )
 
     def clean_start_at(self):
-        """ Проверка выбранного времени кратности 15 минутам """
+        """Проверка выбранного времени кратности 15 минутам"""
         value = self.cleaned_data["start_at"]
         if value.minute % 15 != 0 or value.second != 0 or value.microsecond != 0:
             raise forms.ValidationError("Время начала должно быть кратно 15 минутам.")
         return value
 
     def clean_end_at(self):
-        """ Проверка выбранного времени кратности 15 минутам """
+        """Проверка выбранного времени кратности 15 минутам"""
         value = self.cleaned_data["end_at"]
         if value.minute % 15 != 0 or value.second != 0 or value.microsecond != 0:
             raise forms.ValidationError("Время окончания должно быть кратно 15 минутам.")
         return value
 
     def clean(self):
-        """ Проверка корректности введенных времени (время начала брони не может быть позже времени окончания брони)"""
+        """Проверка корректности введенных времени (время начала брони не может быть позже времени окончания брони)"""
         cleaned_data = super().clean()
         start_at = cleaned_data.get("start_at")
         end_at = cleaned_data.get("end_at")
