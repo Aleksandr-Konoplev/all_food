a59d9703f3de8ac1a202edcf6d963a82334aa895
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-19 19:37:36 +1000

    fix email verification

--- users/services.py
+++ users/services.py
@@ -8,13 +8,6 @@ from users.models import User
 def email_verification(request: HttpRequest, token) -> HttpResponse:
     user = get_object_or_404(User, token=token)
     user.is_active = True
+    user.token = None
     user.save()
     return redirect(reverse("users:login"))
-
-
-def telegram_verification(request: HttpRequest, token) -> HttpResponse:
-    pass
-
-
-def phone_number_verification(request: HttpRequest, token) -> HttpResponse:
-    pass
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.services import email_verification
 from users.views import UserCreateView, UserListView, UserDetailView, UserUpdateView, UserDeleteView
 
 app_name = UsersConfig.name
@@ -10,6 +11,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),  # type: ignore
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserCreateView.as_view(), name='register'),
+    path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
     path('list/', UserListView.as_view(), name='users-list'),
     path('user/<int:pk>/detail/', UserDetailView.as_view(), name='user-detail'),
     path('user/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
