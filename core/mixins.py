9b8fd451c78427499b7c915307265a9af56abec9
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-03-31 18:33:22 +1000

    add promo in base.html

--- core/mixins.py
+++ core/mixins.py
@@ -5,5 +5,6 @@ class AddBaseContentMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)  # noqa
         context['greeting'] = ContentForSite.objects.get(name_tag='greeting')
+        context['promo_1'] = ContentForSite.objects.filter(name_tag='promo_1').first()
         return context
 
