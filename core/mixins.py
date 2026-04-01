bc463318e6711752f8e28c474fe3873eef713eaf
Konoplev Aleksandr <konoplev.a.a0000@gmail.com>
2026-04-02 02:25:35 +1000

    fixed template

--- core/mixins.py
+++ core/mixins.py
@@ -4,7 +4,8 @@ from core.models import ContentForSite
 class AddBaseContentMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)  # noqa
-        context['greeting'] = ContentForSite.objects.get(name_tag='greeting')
+        context['greeting'] = ContentForSite.objects.filter(name_tag='greeting').first()
+        context['main_img'] = ContentForSite.objects.filter(name_tag='main_img').first()
         context['promo_items'] = ContentForSite.objects.filter(name_tag__startswith='promo_').order_by('name_tag')
         return context
 
